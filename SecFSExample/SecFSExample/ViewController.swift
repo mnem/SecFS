5c299225e168a7b70cb6d3730e19c84ae2346e80
David Wagner <mnem@noiseandheat.com>
2016-10-10 21:46:41 +0100

    Initial commit for SecFS consumer.

---
+++ SecFSExample/SecFSExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SecFSExample
+//
+//  Created by David Wagner on 10/10/2016.
+//  Copyright © 2016 David Wagner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
